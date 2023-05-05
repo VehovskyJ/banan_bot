b0f2d73da3535d18a39b34d4ade4dab4a075eee2
Padias <matyas.krejza@seznam.cz>
2023-05-05 22:39:57 +0200

    added jinxed and nerded commands

--- main.go
+++ main.go
@@ -249,6 +249,45 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 	}
+
+	if strings.Contains(strings.ToLower(m.Content), "get nerded") {
+		if len(m.Mentions) == 1 {
+			nerdedMessagesCount := 0
+			nerdedMessagesTBD := 15
+			messages, _ := s.ChannelMessages(m.ChannelID, 100, "", "", "")
+			for _, message := range messages {
+				if message.Author.ID == m.Mentions[0].ID {
+					s.MessageReactionAdd(message.ChannelID, message.ID, "🤓")
+					nerdedMessagesCount++
+					nerdedMessagesTBD--
+					if nerdedMessagesTBD == 0 {
+						break
+					}
+				}
+			}
+
+		}
+	}
+
+	if strings.Contains(strings.ToLower(m.Content), "get jinxed") {
+		if len(m.Mentions) == 1 {
+			nerdedMessagesCount := 0
+			nerdedMessagesTBD := 15
+			messages, _ := s.ChannelMessages(m.ChannelID, 100, "", "", "")
+			for _, message := range messages {
+				if message.Author.ID == m.Mentions[0].ID {
+					s.MessageReactionAdd(message.ChannelID, message.ID, "jinx1:1074460008307245067")
+					nerdedMessagesCount++
+					nerdedMessagesTBD--
+					if nerdedMessagesTBD == 0 {
+						break
+					}
+				}
+			}
+
+		}
+	}
+
 	if strings.ToLower(m.Content) == "opice hovno" {
 		s.ChannelMessageSendReply(m.ChannelID, "ZIJU TI VE ZDECH ZIJU TI VE ZDECH", m.Reference())
 	}
